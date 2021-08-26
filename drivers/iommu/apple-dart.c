f6997232a20b20c00a91d7e7345a0184a15de521
Alyssa Rosenzweig <alyssa@rosenzweig.io>
2021-08-26 13:19:51 -0400

    iommu/dart: Assert !locked when reconfiguring
    Signed-off-by: Alyssa Rosenzweig <alyssa@rosenzweig.io>

--- drivers/iommu/apple-dart.c
+++ drivers/iommu/apple-dart.c
@@ -197,6 +197,7 @@ apple_dart_hw_enable_translation(struct apple_dart_stream_map *stream_map)
 {
 	int sid;
 
+	WARN_ON(stream_map->dart->locked);
 	for_each_set_bit(sid, &stream_map->sidmap, DART_MAX_STREAMS)
 		writel(DART_TCR_TRANSLATE_ENABLE,
 		       stream_map->dart->regs + DART_TCR(sid));
@@ -206,6 +207,7 @@ static void apple_dart_hw_disable_dma(struct apple_dart_stream_map *stream_map)
 {
 	int sid;
 
+	WARN_ON(stream_map->dart->locked);
 	for_each_set_bit(sid, &stream_map->sidmap, DART_MAX_STREAMS)
 		writel(0, stream_map->dart->regs + DART_TCR(sid));
 }
@@ -215,6 +217,7 @@ apple_dart_hw_enable_bypass(struct apple_dart_stream_map *stream_map)
 {
 	int sid;
 
+	WARN_ON(stream_map->dart->locked);
 	WARN_ON(!stream_map->dart->supports_bypass);
 	for_each_set_bit(sid, &stream_map->sidmap, DART_MAX_STREAMS)
 		writel(DART_TCR_BYPASS0_ENABLE | DART_TCR_BYPASS1_ENABLE,
@@ -226,6 +229,7 @@ static void apple_dart_hw_set_ttbr(struct apple_dart_stream_map *stream_map,
 {
 	int sid;
 
+	WARN_ON(stream_map->dart->locked);
 	WARN_ON(paddr & ((1 << DART_TTBR_SHIFT) - 1));
 	for_each_set_bit(sid, &stream_map->sidmap, DART_MAX_STREAMS)
 		writel(DART_TTBR_VALID | (paddr >> DART_TTBR_SHIFT),
@@ -237,6 +241,7 @@ static void apple_dart_hw_clear_ttbr(struct apple_dart_stream_map *stream_map,
 {
 	int sid;
 
+	WARN_ON(stream_map->dart->locked);
 	for_each_set_bit(sid, &stream_map->sidmap, DART_MAX_STREAMS)
 		writel(0, stream_map->dart->regs + DART_TTBR(sid, idx));
 }
