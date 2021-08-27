8eb3f623cd3e4c04bbaa6d0287c2c8ea09dd187e
Alyssa Rosenzweig <alyssa@rosenzweig.io>
2021-08-27 11:19:51 -0400

    WIP: drm/apple: Add DCP display driver
    Add a DRM/KMS driver for Apple system on chips using the DCP
    coprocessor, namely the Apple M1. The DCP was added in Apple A14; this
    driver does not apply to older iDevices.
    
    Signed-off-by: Alyssa Rosenzweig <alyssa@rosenzweig.io>

---
+++ drivers/gpu/drm/apple/dcp.h
@@ -0,0 +1,35 @@
+#include <drm/drm_atomic.h>
+
+struct apple_crtc;
+
+struct apple_connector {
+	struct drm_connector base;
+	bool connected;
+};
+
+#define to_apple_connector(x) container_of(x, struct apple_connector, base)
+
+/*
+ * Table of supported formats, mapping from DRM fourccs to DCP fourccs.
+ *
+ * TODO: Rather than RGB10_A2, macOS uses a biplanar RGB10_A8 format,
+ * corresponding to DCP format "b3a8". Should a DRM format be created for this?
+ *
+ * TODO: DCP supports a large number of YUV formats. Support these.
+ */
+static const struct dcp_format {
+	u32 drm;
+	u32 dcp;
+} dcp_formats[] = {
+	{ DRM_FORMAT_XRGB8888, fourcc_code('A', 'R', 'G', 'B') },
+	{ DRM_FORMAT_ARGB8888, fourcc_code('A', 'R', 'G', 'B') },
+	{ DRM_FORMAT_XBGR8888, fourcc_code('A', 'B', 'G', 'R') },
+	{ DRM_FORMAT_ABGR8888, fourcc_code('A', 'B', 'G', 'R') },
+	{ DRM_FORMAT_BGRA8888, fourcc_code('B', 'G', 'R', 'A') },
+	{ DRM_FORMAT_BGRX8888, fourcc_code('B', 'G', 'R', 'A') },
+};
+
+void dcp_link(struct platform_device *pdev, struct apple_crtc *apple, struct apple_connector *connector);
+void dcp_swap(struct platform_device *pdev, struct drm_atomic_state *state);
+bool dcp_is_initialized(struct platform_device *pdev);
+void apple_crtc_vblank(struct apple_crtc *apple);
