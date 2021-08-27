84fbe6600b34141ce943d6e78b8d6531d26f5c9a
Alyssa Rosenzweig <alyssa@rosenzweig.io>
2021-08-27 11:19:51 -0400

    WIP: drm/apple: Add DCP display driver
    Add a DRM/KMS driver for Apple system on chips using the DCP
    coprocessor, namely the Apple M1. The DCP was added in Apple A14; this
    driver does not apply to older iDevices.
    
    Signed-off-by: Alyssa Rosenzweig <alyssa@rosenzweig.io>

---
+++ drivers/gpu/drm/apple/apple_drv.c
@@ -0,0 +1,377 @@
+// SPDX-License-Identifier: GPL-2.0-only
+/* Copyright 2021 Alyssa Rosenzweig <alyssa@rosenzweig.io> */
+/* Based on meson driver which is
+ * Copyright (C) 2016 BayLibre, SAS
+ * Author: Neil Armstrong <narmstrong@baylibre.com>
+ * Copyright (C) 2015 Amlogic, Inc. All rights reserved.
+ * Copyright (C) 2014 Endless Mobile
+ */
+
+#include <linux/module.h>
+#include <linux/clk.h>
+#include <linux/component.h>
+#include <linux/of_device.h>
+#include <linux/of_graph.h>
+#include <linux/of_reserved_mem.h>
+#include <linux/pm_runtime.h>
+#include <linux/debugfs.h>
+#include <linux/dma-mapping.h>
+
+#include <drm/drm_aperture.h>
+#include <drm/drm_atomic.h>
+#include <drm/drm_atomic_helper.h>
+#include <drm/drm_crtc.h>
+#include <drm/drm_drv.h>
+#include <drm/drm_fb_helper.h>
+#include <drm/drm_fourcc.h>
+#include <drm/drm_fb_cma_helper.h>
+#include <drm/drm_gem_cma_helper.h>
+#include <drm/drm_gem_framebuffer_helper.h>
+#include <drm/drm_modeset_helper.h>
+#include <drm/drm_of.h>
+#include <drm/drm_probe_helper.h>
+
+#define DISP0_FORMAT 0x30
+#define    DISP0_FORMAT_BGRA 0x5000
+#define DISP0_FRAMEBUFFER_0 0x54
+#define DISP0_FRAMEBUFFER_1 0x58
+#define DISP0_FRAMEBUFFER_2 0x5c
+
+struct apple_drm_private {
+	struct drm_device	drm;
+	void __iomem		*regs;
+};
+
+#define to_apple_drm_private(x) \
+	container_of(x, struct apple_drm_private, drm)
+
+DEFINE_DRM_GEM_CMA_FOPS(apple_fops);
+
+static const struct drm_driver apple_drm_driver = {
+	.driver_features = DRIVER_MODESET | DRIVER_GEM | DRIVER_ATOMIC,
+	.name = "apple",
+	.desc = "Apple Display Controller DRM driver",
+	.date = "20210801",
+	.major = 1,
+	.minor = 0,
+	.patchlevel = 0,
+	.fops = &apple_fops,
+	DRM_GEM_CMA_DRIVER_OPS,
+};
+
+static int apple_plane_atomic_check(struct drm_plane *plane,
+				    struct drm_atomic_state *state)
+{
+	/* TODO */
+	return 0;
+
+}
+
+static void apple_plane_atomic_disable(struct drm_plane *plane,
+				       struct drm_atomic_state *state)
+{
+	/* TODO */
+}
+
+static void apple_plane_atomic_update(struct drm_plane *plane,
+				      struct drm_atomic_state *state)
+{
+	struct apple_drm_private *apple = to_apple_drm_private(plane->dev);
+	struct drm_plane_state *plane_state;
+	struct drm_framebuffer *fb;
+	dma_addr_t dva;
+
+	plane_state = drm_atomic_get_new_plane_state(state, plane);
+	fb = plane_state->fb;
+	dva = drm_fb_cma_get_gem_addr(fb, plane_state, 0);
+
+	writel(dva, apple->regs + DISP0_FRAMEBUFFER_0);
+	writel(DISP0_FORMAT_BGRA, apple->regs + DISP0_FORMAT);
+}
+
+static const struct drm_plane_helper_funcs apple_plane_helper_funcs = {
+	.atomic_check	= apple_plane_atomic_check,
+	.atomic_disable	= apple_plane_atomic_disable,
+	.atomic_update	= apple_plane_atomic_update,
+};
+
+static const struct drm_plane_funcs apple_plane_funcs = {
+	.update_plane		= drm_atomic_helper_update_plane,
+	.disable_plane		= drm_atomic_helper_disable_plane,
+	.destroy		= drm_plane_cleanup,
+	.reset			= drm_atomic_helper_plane_reset,
+	.atomic_duplicate_state = drm_atomic_helper_plane_duplicate_state,
+	.atomic_destroy_state	= drm_atomic_helper_plane_destroy_state,
+};
+
+uint32_t apple_plane_formats[] = {
+	/* TODO: More formats */
+	DRM_FORMAT_XRGB8888,
+	DRM_FORMAT_ARGB8888,
+};
+
+uint64_t apple_format_modifiers[] = {
+	DRM_FORMAT_MOD_LINEAR,
+	DRM_FORMAT_MOD_INVALID
+};
+
+struct drm_plane *apple_plane_init(struct drm_device *dev)
+{
+	int ret;
+	struct drm_plane *plane;
+
+	plane = devm_kzalloc(dev->dev, sizeof(*plane), GFP_KERNEL);
+
+	ret = drm_universal_plane_init(dev, plane, 0x1, &apple_plane_funcs,
+				       apple_plane_formats,
+				       ARRAY_SIZE(apple_plane_formats),
+				       apple_format_modifiers,
+				       DRM_PLANE_TYPE_PRIMARY, NULL);
+
+	drm_plane_helper_add(plane, &apple_plane_helper_funcs);
+
+	if (ret)
+		return ERR_PTR(ret);
+
+	return plane;
+}
+
+static const struct drm_crtc_funcs apple_crtc_funcs = {
+	.atomic_destroy_state	= drm_atomic_helper_crtc_destroy_state,
+	.atomic_duplicate_state = drm_atomic_helper_crtc_duplicate_state,
+	.destroy		= drm_crtc_cleanup,
+	.page_flip		= drm_atomic_helper_page_flip,
+	.reset			= drm_atomic_helper_crtc_reset,
+	.set_config             = drm_atomic_helper_set_config,
+};
+
+static void apple_encoder_destroy(struct drm_encoder *encoder)
+{
+	drm_encoder_cleanup(encoder);
+}
+
+static const struct drm_encoder_funcs apple_encoder_funcs = {
+	.destroy        = apple_encoder_destroy,
+};
+
+static const struct drm_mode_config_funcs apple_mode_config_funcs = {
+	.atomic_check        = drm_atomic_helper_check,
+	.atomic_commit       = drm_atomic_helper_commit,
+	.fb_create           = drm_gem_fb_create,
+};
+
+static const struct drm_mode_config_helper_funcs apple_mode_config_helpers = {
+	.atomic_commit_tail = drm_atomic_helper_commit_tail_rpm,
+};
+
+static void apple_connector_destroy(struct drm_connector *connector)
+{
+	drm_connector_cleanup(connector);
+}
+
+static enum drm_connector_status
+apple_connector_detect(struct drm_connector *connector, bool force)
+{
+	/* TODO: stub */
+	return connector_status_connected;
+}
+
+static const struct drm_connector_funcs apple_connector_funcs = {
+	.detect			= apple_connector_detect,
+	.fill_modes		= drm_helper_probe_single_connector_modes,
+	.destroy		= apple_connector_destroy,
+	.reset			= drm_atomic_helper_connector_reset,
+	.atomic_duplicate_state	= drm_atomic_helper_connector_duplicate_state,
+	.atomic_destroy_state	= drm_atomic_helper_connector_destroy_state,
+};
+
+static int apple_connector_get_modes(struct drm_connector *connector)
+{
+	struct drm_device *dev = connector->dev;
+	struct drm_display_mode *mode;
+
+	/* STUB */
+
+	struct drm_display_mode dummy = {
+		DRM_SIMPLE_MODE(1920, 1080, 508, 286),
+	};
+
+	dummy.clock = 60 * dummy.hdisplay * dummy.vdisplay;
+	drm_mode_set_name(&dummy);
+
+	mode = drm_mode_duplicate(dev, &dummy);
+	if (!mode) {
+		DRM_ERROR("Failed to create a new display mode\n");
+		return 0;
+	}
+
+	drm_mode_probed_add(connector, mode);
+	return 1;
+}
+
+static int apple_connector_mode_valid(struct drm_connector *connector,
+					   struct drm_display_mode *mode)
+{
+	/* STUB */
+	return MODE_OK;
+}
+
+static const
+struct drm_connector_helper_funcs apple_connector_helper_funcs = {
+	.get_modes	= apple_connector_get_modes,
+	.mode_valid	= apple_connector_mode_valid,
+};
+
+static void apple_crtc_atomic_enable(struct drm_crtc *crtc,
+				     struct drm_atomic_state *state)
+{
+	/* TODO */
+}
+
+static void apple_crtc_atomic_disable(struct drm_crtc *crtc,
+				      struct drm_atomic_state *state)
+{
+	/* TODO */
+}
+
+static void apple_crtc_atomic_begin(struct drm_crtc *crtc,
+				    struct drm_atomic_state *state)
+{
+	/* TODO */
+}
+
+static void apple_crtc_atomic_flush(struct drm_crtc *crtc,
+				    struct drm_atomic_state *state)
+{
+	/* TODO */
+}
+
+static const struct drm_crtc_helper_funcs apple_crtc_helper_funcs = {
+	.atomic_begin	= apple_crtc_atomic_begin,
+	.atomic_flush	= apple_crtc_atomic_flush,
+	.atomic_enable	= apple_crtc_atomic_enable,
+	.atomic_disable	= apple_crtc_atomic_disable,
+};
+
+static int apple_platform_probe(struct platform_device *pdev)
+{
+	struct apple_drm_private *apple;
+	struct drm_plane *plane;
+	struct drm_crtc *crtc;
+	struct drm_encoder *encoder;
+	struct drm_connector *connector;
+	int ret;
+
+	apple = devm_drm_dev_alloc(&pdev->dev, &apple_drm_driver,
+				   struct apple_drm_private, drm);
+	if (IS_ERR(apple))
+		return PTR_ERR(apple);
+
+	ret = dma_set_mask_and_coherent(&pdev->dev, DMA_BIT_MASK(64));
+
+	if (ret)
+		return ret;
+
+	apple->regs = devm_platform_ioremap_resource(pdev, 0);
+
+	if (!apple->regs)
+		return -ENODEV;
+
+	/*
+	 * Remove early framebuffers (ie. simplefb). The framebuffer can be
+	 * located anywhere in RAM
+	 */
+	ret = drm_aperture_remove_framebuffers(false, &apple_drm_driver);
+	if (ret)
+		return ret;
+
+	ret = drmm_mode_config_init(&apple->drm);
+	if (ret)
+		goto err_unload;
+
+	apple->drm.mode_config.max_width = 3840;
+	apple->drm.mode_config.max_height = 2160;
+	apple->drm.mode_config.funcs = &apple_mode_config_funcs;
+	apple->drm.mode_config.helper_private	= &apple_mode_config_helpers;
+
+	plane = apple_plane_init(&apple->drm);
+
+	if (IS_ERR(plane)) {
+		ret = PTR_ERR(plane);
+		goto err_unload;
+	}
+
+	crtc = devm_kzalloc(&pdev->dev, sizeof(*crtc), GFP_KERNEL);
+	ret = drm_crtc_init_with_planes(&apple->drm, crtc, plane, NULL,
+					&apple_crtc_funcs, NULL);
+	if (ret)
+		goto err_unload;
+
+
+	drm_crtc_helper_add(crtc, &apple_crtc_helper_funcs);
+
+	encoder = devm_kzalloc(&pdev->dev, sizeof(*encoder), GFP_KERNEL);
+	encoder->possible_crtcs = drm_crtc_mask(crtc);
+	ret = drm_encoder_init(&apple->drm, encoder, &apple_encoder_funcs,
+			       DRM_MODE_ENCODER_TMDS /* XXX */, "apple_hdmi");
+	if (ret)
+		goto err_unload;
+
+	connector = devm_kzalloc(&pdev->dev, sizeof(*connector), GFP_KERNEL);
+
+	drm_connector_helper_add(connector,
+			&apple_connector_helper_funcs);
+
+	ret = drm_connector_init(&apple->drm, connector, &apple_connector_funcs,
+				 DRM_MODE_CONNECTOR_HDMIA);
+	if (ret)
+		goto err_unload;
+
+	ret = drm_connector_attach_encoder(connector, encoder);
+	if (ret)
+		goto err_unload;
+
+	drm_mode_config_reset(&apple->drm); // TODO: needed?
+
+	ret = drm_dev_register(&apple->drm, 0);
+	if (ret)
+		goto err_unload;
+
+	drm_fbdev_generic_setup(&apple->drm, 32);
+
+	return 0;
+
+err_unload:
+	drm_dev_put(&apple->drm);
+	return ret;
+}
+
+static int apple_platform_remove(struct platform_device *pdev)
+{
+	struct drm_device *drm = platform_get_drvdata(pdev);
+
+	drm_dev_unregister(drm);
+
+	return 0;
+}
+
+static const struct of_device_id of_match[] = {
+	{ .compatible = "apple,t8103-dcp" },
+	{}
+};
+MODULE_DEVICE_TABLE(of, of_match);
+
+static struct platform_driver apple_platform_driver = {
+	.probe		= apple_platform_probe,
+	.remove		= apple_platform_remove,
+	.driver	= {
+		.name = "apple",
+		.of_match_table	= of_match,
+	},
+};
+
+module_platform_driver(apple_platform_driver);
+
+MODULE_AUTHOR("Alyssa Rosenzweig <alyssa@rosenzweig.io>");
+MODULE_DESCRIPTION("Apple Display Controller DRM driver");
+MODULE_LICENSE("GPL v2");
---
+++ drivers/gpu/drm/apple/dcp.c
@@ -0,0 +1,271 @@
+// SPDX-License-Identifier: GPL-2.0-only
+/* Copyright 2021 Alyssa Rosenzweig <alyssa@rosenzweig.io> */
+
+#include <linux/module.h>
+#include <linux/clk.h>
+#include <linux/component.h>
+#include <linux/of_device.h>
+#include <linux/of_graph.h>
+#include <linux/of_reserved_mem.h>
+#include <linux/pm_runtime.h>
+#include <linux/debugfs.h>
+#include <linux/dma-mapping.h>
+#include <linux/apple-mailbox.h>
+#include <linux/apple-rtkit.h>
+
+#include "dcpep.h"
+
+struct apple_dcp {
+	struct device *dev;
+	struct apple_rtkit *rtk;
+
+	/* DCP shared memory */
+	void *shmem;
+
+	/* Active contexts indexed by ID */
+	struct dcp_context contexts[DCP_NUM_CONTEXTS];
+};
+
+static enum dcp_context_id dcp_context_get_id(struct dcp_context *ctx)
+{
+	struct apple_dcp *dcp = ctx->dcp;
+	struct dcp_context *base = &dcp->contexts[0];
+
+	return ctx - base;
+}
+
+static void dcpep_send(struct apple_dcp *dcp, uint64_t msg)
+{
+	dev_info(dcp->dev, "-> %llx\n", msg);
+	apple_rtkit_send_message(dcp->rtk, DCP_ENDPOINT, msg);
+}
+
+void *dcp_push_packet(struct dcp_context *ctx, char tag[4], u32 in_len,
+		      u32 out_len, void *data, bool ack)
+{
+	struct dcp_packet_header header = {
+		.in_len = in_len,
+		.out_len = out_len,
+
+		/* Tag is reversed due to endianness of the fourcc */
+		.tag[0] = tag[3],
+		.tag[1] = tag[2],
+		.tag[2] = tag[1],
+		.tag[3] = tag[0],
+	};
+
+	enum dcp_context_id id = dcp_context_get_id(ctx);
+	u16 offset = ctx->offset;
+	void *out = ctx->buf + offset;
+
+	void *out_data = out + sizeof(header);
+	size_t data_len = sizeof(header) + in_len + out_len;
+	size_t copy_len = ack ? out_len : in_len;
+
+	if (ack)
+		out_data += in_len;
+
+	memcpy(out, &header, sizeof(header));
+
+	WARN_ON((data == NULL) != (copy_len == 0));
+
+	if (data)
+		memcpy(out_data, data, copy_len);
+
+	dcpep_send(ctx->dcp, dcpep_msg(id, data_len, offset, ack));
+
+	ctx->offset += ALIGN(data_len, DCP_PACKET_ALIGNMENT);
+	return out + sizeof(header) + in_len;
+}
+
+void dcp_ack(struct dcp_context *ctx)
+{
+	enum dcp_context_id id = dcp_context_get_id(ctx);
+
+	dcpep_send(ctx->dcp, dcpep_msg(id, 0, 0, true));
+}
+
+static struct dcp_context *dcp_get_context(struct apple_dcp *dcp,
+					   enum dcp_context_id id)
+{
+	struct dcp_context *ctx;
+
+	if (id >= DCP_NUM_CONTEXTS)
+		return NULL;
+
+	ctx = &dcp->contexts[id];
+
+	if (!ctx->buf)
+		return NULL;
+
+	WARN_ON(ctx->dcp != dcp);
+
+	return ctx;
+}
+
+static void dcpep_got_msg(struct apple_dcp *dcp, u64 message)
+{
+	struct dcp_context *ctx;
+	void *data;
+
+	bool ack;
+	enum dcp_context_id ctx_id;
+	u16 offset;
+	u32 length;
+
+	ack = message & BIT(DCPEP_ACK_SHIFT);
+	ctx_id = (message & DCPEP_CONTEXT_MASK) >> DCPEP_CONTEXT_SHIFT;
+	offset = (message & DCPEP_OFFSET_MASK) >> DCPEP_OFFSET_SHIFT;
+	length = (message >> DCPEP_LENGTH_SHIFT);
+
+	ctx = dcp_get_context(dcp, ctx_id);
+
+	if (!ctx) {
+		dev_warn(dcp->dev, "invalid context received %u", ctx_id);
+		return;
+	}
+
+	data = ctx->buf + offset;
+
+	dev_info(dcp->dev, "got %s to context %u offset %u length %u\n",
+		 ack ? "ack" : "message", ctx_id, offset, length);
+
+	print_hex_dump(KERN_INFO, "apple-dcp: ", DUMP_PREFIX_OFFSET, 16, 1,
+			data, length, true);
+
+	if (!ack)
+		dcp_ack(ctx);
+}
+
+static void dcp_start_signal(struct apple_dcp *dcp)
+{
+	struct dcp_context *ctx = &dcp->contexts[DCP_CONTEXT_CMD];
+	uint32_t *resp;
+
+	resp = dcp_push_packet(ctx, "A401", 0, sizeof(resp), NULL, false);
+	dev_info(dcp->dev, "Started the DCP, resp %u\n", *resp);
+}
+
+static void dcp_got_msg(void *cookie, u8 endpoint, u64 message)
+{
+	struct apple_dcp *dcp = cookie;
+	enum dcpep_type type;
+
+	WARN_ON(endpoint != DCP_ENDPOINT);
+
+	dev_info(dcp->dev, "<- %llx\n", message);
+
+	type = (message >> DCPEP_TYPE_SHIFT) & DCPEP_TYPE_MASK;
+
+	switch (type) {
+	case DCPEP_TYPE_INITIALIZED:
+		dev_info(dcp->dev, "initialized!\n");
+		dcp_start_signal(dcp);
+		break;
+
+	case DCPEP_TYPE_MESSAGE:
+		dcpep_got_msg(dcp, message);
+		break;
+
+	default:
+		dev_warn(dcp->dev, "Ignoring unknown type %u in message %llx\n",
+			 type, message);
+		return;
+	}
+}
+
+static int dummy_shmem_verify(void *cookie, dma_addr_t addr, size_t len)
+{
+	return 0;
+}
+
+static struct apple_rtkit_ops rtkit_ops = {
+	.shmem_owner = APPLE_RTKIT_SHMEM_OWNER_LINUX,
+	.shmem_verify = dummy_shmem_verify,
+	.recv_message = dcp_got_msg,
+};
+
+static void dcp_setup_context(struct apple_dcp *dcp, enum dcp_context_id id,
+			      u32 start)
+{
+	dcp->contexts[id] = (struct dcp_context) {
+		.dcp = dcp,
+		.buf = dcp->shmem + start,
+		.offset = 0
+	};
+}
+
+static int dcp_platform_probe(struct platform_device *pdev)
+{
+	struct device *dev = &pdev->dev;
+	struct resource *res;
+	struct apple_dcp *dcp;
+	dma_addr_t shmem_iova;
+	int ret;
+
+	dcp = devm_kzalloc(dev, sizeof(*dcp), GFP_KERNEL);
+	if (!dcp)
+		return -ENOMEM;
+
+	platform_set_drvdata(pdev, dcp);
+
+	dcp->dev = dev;
+
+	ret = dma_set_mask_and_coherent(dev, DMA_BIT_MASK(64));
+
+	res = platform_get_resource_byname(pdev, IORESOURCE_MEM, "coproc");
+	if (!res)
+		return -EINVAL;
+
+	dcp->rtk = apple_rtkit_init(dev, dcp, res, "mbox", &rtkit_ops);
+	apple_rtkit_boot_wait(dcp->rtk);
+	apple_rtkit_start_ep(dcp->rtk, DCP_ENDPOINT);
+
+	dcp->shmem = dma_alloc_coherent(dev, DCP_SHMEM_SIZE, &shmem_iova,
+					GFP_KERNEL);
+	dev_info(dev, "shmem allocated at dva %x\n", (u32) shmem_iova);
+
+	dcp_setup_context(dcp, DCP_CONTEXT_CB, 0x60000);
+	dcp_setup_context(dcp, DCP_CONTEXT_CMD, 0);
+	dcp_setup_context(dcp, DCP_CONTEXT_ASYNC, 0x40000);
+	dcp_setup_context(dcp, DCP_CONTEXT_OOBCB, 0x68000);
+	dcp_setup_context(dcp, DCP_CONTEXT_OOBCMD, 0x8000);
+
+	dcpep_send(dcp, dcpep_set_shmem(shmem_iova));
+
+	if (ret)
+		return ret;
+
+	return ret;
+}
+
+static int dcp_platform_remove(struct platform_device *pdev)
+{
+#if 0
+	struct apple_dcp *dcp = platform_get_drvdata(pdev);
+
+	apple_rtkit_free(dcp->rtk);
+#endif
+	return 0;
+}
+
+static const struct of_device_id of_match[] = {
+	{ .compatible = "apple,dcp" },
+	{}
+};
+MODULE_DEVICE_TABLE(of, of_match);
+
+static struct platform_driver apple_platform_driver = {
+	.probe		= dcp_platform_probe,
+	.remove		= dcp_platform_remove,
+	.driver	= {
+		.name = "apple-dcp",
+		.of_match_table	= of_match,
+	},
+};
+
+module_platform_driver(apple_platform_driver);
+
+MODULE_AUTHOR("Alyssa Rosenzweig <alyssa@rosenzweig.io>");
+MODULE_DESCRIPTION("Apple Display Controller DRM driver");
+MODULE_LICENSE("GPL v2");
