49894793cf816dc53b732b6404d7b9f866d99942
Alyssa Rosenzweig <alyssa@rosenzweig.io>
2021-09-01 16:07:21 -0400

    Add more formats and commentary

--- drivers/gpu/drm/apple/dcp.h
+++ drivers/gpu/drm/apple/dcp.h
@@ -2,12 +2,24 @@
 
 struct apple_drm_private;
 
+/*
+ * Table of supported formats, mapping from DRM fourccs to DCP fourccs.
+ *
+ * TODO: Rather than RGB10_A2, macOS uses a biplanar RGB10_A8 format,
+ * corresponding to DCP format "b3a8". Should a DRM format be created for this?
+ *
+ * TODO: DCP supports a large number of YUV formats. Support these.
+ */
 static const struct dcp_format {
 	u32 drm;
 	char dcp[4];
 } dcp_formats[] = {
 	{ DRM_FORMAT_XRGB8888, "BGRA" },
 	{ DRM_FORMAT_ARGB8888, "BGRA" },
+	{ DRM_FORMAT_XBGR8888, "RGBA" },
+	{ DRM_FORMAT_ABGR8888, "RGBA" },
+	{ DRM_FORMAT_BGRA8888, "ARGB" },
+	{ DRM_FORMAT_BGRX8888, "ARGB" },
 };
 
 void dcp_link(struct platform_device *pdev, struct apple_drm_private *apple);
