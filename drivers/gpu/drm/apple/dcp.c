f94c9cc15d642ebb48392849e086727768cc6e0a
Alyssa Rosenzweig <alyssa@rosenzweig.io>
2021-09-01 17:16:38 -0400

    tighten

--- drivers/gpu/drm/apple/dcp.c
+++ drivers/gpu/drm/apple/dcp.c
@@ -396,10 +396,7 @@ static bool dcpep_cb_map_physical(struct apple_dcp *dcp, void *out, void *in)
 	return true;
 }
 
-/*
- * Pixel clock frequency in Hz. This is 533.333328 Mhz, factored as 33.333333
- * MHz * 16. Slightly greater than the 4K@60 VGA pixel clock 533.250 MHz.
- */
+/* Pixel clock frequency in Hz, a bit more than 4K@60 VGA clock 533.250 MHz */
 #define DCP_PIXEL_CLOCK (533333328)
 
 static bool dcpep_cb_get_frequency(struct apple_dcp *dcp, void *out, void *in)
