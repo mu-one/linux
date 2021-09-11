e3768a0eadb6ca73267e21eec0f7e83ab4792098
Alyssa Rosenzweig <alyssa@rosenzweig.io>
2021-09-11 18:52:10 -0400

    ohh

--- drivers/gpu/drm/apple/dcp.c
+++ drivers/gpu/drm/apple/dcp.c
@@ -526,7 +526,7 @@ static bool dcpep_cb_rt_bandwidth_setup(struct apple_dcp *dcp, void *out, void *
 		.reg_doorbell = 0x23BC3C000, // reg[6] in disp0/dispext0 - part of pmp/pmgr
 		.doorbell_bit = 2,
 //		.padding[1] = 0x43FB2690,
-		.padding[2] = 0xFFFFFFFF,
+//		.padding[2] = 0xFFFFFFFF,
 		.padding[3] = 0x4,
 		.padding[4] = 0x0,
 		.padding[5] = 0x465,
